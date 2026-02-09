5f620c78296f99aa603be246b7792f2fdcc7480e
Takako Nagamatsu <takako_nagamatsu@jibun-note.co.jp>
2026-02-09 10:13:05 +0900

    スタックカードが重なるよう修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({
     children: React.ReactNode
 }) {
     return (
-        <div className="flex flex-col min-h-screen overflow-x-hidden">
+        <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow min-w-0">{children}</main>
             <Footer />
