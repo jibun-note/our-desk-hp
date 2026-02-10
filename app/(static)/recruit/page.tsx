99ba2eb6269f710fb7ba329aa668e260f85a420e
Takako Nagamatsu <takako_nagamatsu@jibun-note.co.jp>
2026-02-10 15:47:53 +0900

    Recruitページ　モック案作成

--- app/(static)/recruit/page.tsx
+++ app/(static)/recruit/page.tsx
@@ -1,4 +1,4 @@
-import ComingSoonBlock from '@/components/sections/ComingSoonBlock'
+import RecruitContent from '@/components/sections/RecruitContent'
 import HeroSection from '@/components/sections/HeroSection'
 import BreadcrumbJsonLdServer from '@/components/seo/BreadcrumbJsonLdServer'
 import { createPageMetadata } from '@/lib/seo'
@@ -14,7 +14,7 @@ export default function RecruitPage() {
         <>
             <BreadcrumbJsonLdServer path="/recruit/" name="Recruit" />
             <HeroSection title="Recruit" description="採用情報" />
-            <ComingSoonBlock />
+            <RecruitContent />
         </>
     )
 }
